1e527527b8877ab2fd687f0084e2c1f78358e628
TurronDeCodigo <Andresicokk@gmail.com>
2024-04-14 16:48:26 -0500

    canicasPorFrasco

--- src/main/scala/Canicas/package.scala
+++ src/main/scala/Canicas/package.scala
@@ -4,10 +4,19 @@ package object Canicas {
   type Distr = List[Frasco]
 
   def canicasPosiblesFrasco(f:Int, c:Int): List[Frasco] ={
-
     val lista = for{
       n <- 0 until c+1
     } yield ((f,n))
     lista.toList
+  }
+
+  def canicasPorFrasco(n:Int, c:Int): List[Distr]={
+
+    val lista = for{
+      x <- 1 until n+1
+    } yield (canicasPosiblesFrasco(x,c))
+
+    lista.toList
+
   }
 }
